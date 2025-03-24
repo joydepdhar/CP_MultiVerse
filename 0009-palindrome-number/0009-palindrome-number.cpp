38e540720a9f5e469119f25fab536eb4424fcf9d
Joydep Dhar <77049303+joydepdhar@users.noreply.github.com>
2025-03-24 21:13:27 +0600

    Time: 0 ms (100%), Space: 8.5 MB (92.26%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int a = x;
-        long int res = 0;
-        int y;
-        if(a<0) return false;
-        while(a>0)
-        {
-            y=a%10;
-            res = res*10 +y;
-            a = a/10;
-        } 
-        return x==res;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 };
